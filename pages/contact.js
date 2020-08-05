96eb46a38e00de06c094bfa920fbc4ca110bca15
JoseLRiano <joselriano@yahoo.com>
2020-08-05 13:30:02 -0400

    first commit

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+
+const ContactPage = () => {
+    return (
+        <>
+            <h1>Don't Contact me Yet!!!</h1>
+        </>
+    );
+}
+export default ContactPage;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+
+const Index = () => {
+    console.log("*******RUNNING INDEX COMPONENT!*******")
+    return (  
+    <div>
+        <h1>Our Index page</h1>
+    </div>
+);
+}
+export default Index;
